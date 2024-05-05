e3cc040c67bbc760c77ec1e0e85f29e2f0fc55a6
vains <1768396991@163.com>
2024-05-05 16:58:05 +0800

    清空仓库

--- src/util/http/LoginRequest.ts
+++
@@ -1,9 +0,0 @@
-import Request from './Http'
-
-const loginRequest = new Request({
-    // 认证服务地址
-    baseURL: import.meta.env.VITE_OAUTH_ISSUER,
-    timeout: 60 * 1000
-})
-
-export default loginRequest
--- src/util/http/Reqeust.ts
+++
@@ -1,9 +0,0 @@
-import Request from './Http'
-
-const request = new Request({
-    // 网关或其它后端服务地址
-    baseURL: '',
-    timeout: 60 * 1000
-})
-
-export default request
